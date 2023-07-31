ec2cf9afb4697db0048324450386ea225448e012
Nilam Asawale <97042580+Nilamasawale@users.noreply.github.com>
2023-07-31 09:54:45 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<style>
+.column{
+float:left;
+width:50%;
+}
+.row:after{
+content:"";
+display:table;
+clear:both;
+}
+font{
+font-weight:bold;
+}
+</style>
+
+</head>
+<body>
+<div class="row">
+<div align="right" class="column" width="90%">
+<br><b><font size=6 style="background-color:white;color:green;width:90%;height:15%;">ADMISSION  MANAGEMEN  SYSTEM</font></b>
+</div>
+<div align="right" class="column" width="10%">
+<form method="post" action="logout.php">
+<input type="submit" name="logout"  value="logout" style="background-color:green;color:white;height:40px;width:125px;text-align:center;border-radius:25px;">
+</form>
+</div>
+</div>
+</body>
+</html>
